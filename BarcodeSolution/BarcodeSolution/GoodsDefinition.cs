4d7f1fd8dedef18540a1448ff8176a79be3d4908
ali sharafzade(Personal pc) <ali.sharafzadeh1991@outlook.com>
2019-05-06 19:43:02 +0430

    كاميت دوم

---
+++ BarcodeSolution/BarcodeSolution/GoodsDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeSolution
+{
+    class GoodsDefinition : System.Windows.Forms.Form
+    {
+        public GoodsDefinition()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GoodsDefinition
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Name = "GoodsDefinition";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "تعريف كالا";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
--- BarcodeSolution/BarcodeSolution/MainForm.cs
+++ BarcodeSolution/BarcodeSolution/MainForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace BarcodeSolution
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+        private void GoodsDefine_Click(object sender, EventArgs e)
+        {
+            GoodsDefinition MyForm = new GoodsDefinition();
+            MyForm.ShowDialog();
         }
     }
 }
