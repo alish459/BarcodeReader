341460c128cb75d180f770d092ee1f1df7df5b9d
ali sharafzade(Personal pc) <ali.sharafzadeh1991@outlook.com>
2019-05-06 20:35:21 +0430

    commite

--- BarcodeSolution/BarcodeSolution/GoodsDefinition.cs
+++ BarcodeSolution/BarcodeSolution/GoodsDefinition.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BarcodeSolution
 {
@@ -27,25 +28,43 @@ namespace BarcodeSolution
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel pnlMain;
-
+        private List<Connection.Model.Inventory> Result = new List<Connection.Model.Inventory>();
         public GoodsDefinition()
         {
             InitializeComponent();
+            LoadData();
+            SetGrid();
+        }
+        private void LoadData()
+        {
+            Result = Connection.CrudService.InventoryCrud.ReturnAllGoods();
+            dataGridView1.DataSource = Result;
+        }
+        private void SetGrid()
+        {
+            foreach (DataGridViewColumn item in dataGridView1.Columns)
+            {
+                item.Visible = false;
+            }
+            dataGridView1.Columns["Name"].Visible = true;
+            dataGridView1.Columns["Barcode"].Visible = true;
+
+            dataGridView1.Columns["Name"].HeaderText = "نام كالا";
+            dataGridView1.Columns["Barcode"].HeaderText = "باركد";
+
+            dataGridView1.AutoGenerateColumns = false;
         }
         private void InitializeComponent()
         {
             this.pnlTop = new System.Windows.Forms.Panel();
-            this.pnlFooter = new System.Windows.Forms.Panel();
-            this.pnlMain = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtNaka = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.txtBarcode = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtNaka = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlFooter = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -53,12 +72,15 @@ namespace BarcodeSolution
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pnlTop.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.pnlFooter.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.pnlMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlTop
@@ -74,65 +96,37 @@ namespace BarcodeSolution
             this.pnlTop.Size = new System.Drawing.Size(584, 76);
             this.pnlTop.TabIndex = 0;
             // 
-            // pnlFooter
+            // panel1
             // 
-            this.pnlFooter.Controls.Add(this.panel2);
-            this.pnlFooter.Controls.Add(this.textBox2);
-            this.pnlFooter.Controls.Add(this.label3);
-            this.pnlFooter.Controls.Add(this.textBox3);
-            this.pnlFooter.Controls.Add(this.label4);
-            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlFooter.Location = new System.Drawing.Point(0, 289);
-            this.pnlFooter.Name = "pnlFooter";
-            this.pnlFooter.Size = new System.Drawing.Size(584, 72);
-            this.pnlFooter.TabIndex = 1;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(384, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(195, 33);
+            this.panel1.TabIndex = 4;
             // 
-            // pnlMain
+            // button2
             // 
-            this.pnlMain.Controls.Add(this.textBox1);
-            this.pnlMain.Controls.Add(this.dataGridView1);
-            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlMain.Location = new System.Drawing.Point(0, 76);
-            this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(584, 213);
-            this.pnlMain.TabIndex = 2;
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(17, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "انصراف";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Delete_Click);
             // 
-            // dataGridView1
+            // button1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(584, 213);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(584, 26);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(516, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "نام كالا :";
-            // 
-            // txtNaka
-            // 
-            this.txtNaka.Location = new System.Drawing.Point(328, 8);
-            this.txtNaka.Name = "txtNaka";
-            this.txtNaka.Size = new System.Drawing.Size(182, 26);
-            this.txtNaka.TabIndex = 1;
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(105, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ثبت";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Save_Click);
             // 
             // txtBarcode
             // 
@@ -150,35 +144,34 @@ namespace BarcodeSolution
             this.label2.TabIndex = 2;
             this.label2.Text = "باركد :";
             // 
-            // panel1
+            // txtNaka
             // 
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(384, 40);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(195, 33);
-            this.panel1.TabIndex = 4;
+            this.txtNaka.Location = new System.Drawing.Point(328, 8);
+            this.txtNaka.Name = "txtNaka";
+            this.txtNaka.Size = new System.Drawing.Size(182, 26);
+            this.txtNaka.TabIndex = 1;
             // 
-            // button1
+            // label1
             // 
-            this.button1.BackColor = System.Drawing.Color.LimeGreen;
-            this.button1.Location = new System.Drawing.Point(105, 1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ثبت";
-            this.button1.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(516, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "نام كالا :";
             // 
-            // button2
+            // pnlFooter
             // 
-            this.button2.BackColor = System.Drawing.Color.Crimson;
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Location = new System.Drawing.Point(17, 1);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 29);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "انصراف";
-            this.button2.UseVisualStyleBackColor = false;
+            this.pnlFooter.Controls.Add(this.panel2);
+            this.pnlFooter.Controls.Add(this.textBox2);
+            this.pnlFooter.Controls.Add(this.label3);
+            this.pnlFooter.Controls.Add(this.textBox3);
+            this.pnlFooter.Controls.Add(this.label4);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 289);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(584, 72);
+            this.pnlFooter.TabIndex = 1;
             // 
             // panel2
             // 
@@ -242,6 +235,37 @@ namespace BarcodeSolution
             this.label4.TabIndex = 5;
             this.label4.Text = "نام كالا :";
             // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.dataGridView1);
+            this.pnlMain.Controls.Add(this.textBox1);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 76);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(584, 213);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(584, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(584, 187);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // GoodsDefinition
             // 
             this.ClientSize = new System.Drawing.Size(584, 361);
@@ -254,15 +278,31 @@ namespace BarcodeSolution
             this.Text = "تعريف كالا";
             this.pnlTop.ResumeLayout(false);
             this.pnlTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.pnlFooter.ResumeLayout(false);
             this.pnlFooter.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.pnlMain.ResumeLayout(false);
             this.pnlMain.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
+        }
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (Connection.CrudService.InventoryCrud.Create(new Connection.Model.Inventory() { Barcode = txtBarcode.Text, Name = txtNaka.Text }))
+            {
+                MessageBox.Show("ثبت با موفقيت انجام شد");
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("ثبت با خطا مواجه شد", "پيغام", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
---
+++ BarcodeSolution/Connection/CrudService/InventoryCrud.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection.CrudService
+{
+    public class InventoryCrud
+    {
+        public static List<Connection.Model.Inventory> ReturnAllGoods()
+        {
+            using (var context = new Connection.Model.BarcodeEntity())
+            {
+                return context.Inventory.AsNoTracking().ToList();
+            }
+        }
+        public static bool Create(Connection.Model.Inventory AllGoodsInstance)
+        {
+            using (var context = new Connection.Model.BarcodeEntity())
+            {
+                try
+                {
+                    context.Inventory.Add(AllGoodsInstance);
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.ToString());
+                    return false;
+                }
+            }
+        }
+        public static bool Update(Connection.Model.Inventory ObjectName)
+        {
+            using (var context = new Connection.Model.BarcodeEntity())
+            {
+                try
+                {
+                    var Ins = context.Inventory.Where(a => a.GoodsID == ObjectName.GoodsID).FirstOrDefault();
+                    Ins.Name = ObjectName.Name;
+                    Ins.Barcode = ObjectName.Barcode;
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.ToString());
+                    return false;
+                }
+            }
+        }
+        public static bool Delete(int ID)
+        {
+            using (var context = new Connection.Model.BarcodeEntity())
+            {
+                try
+                {
+                    context.Inventory.Remove(context.Inventory.Find(ID));
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.ToString());
+                    return false;
+                }
+            }
+        }
+    }
+}
